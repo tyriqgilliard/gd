999faa4de08d479a0f6929283f71f7a1db8a6ebc
Gilliard <sgityriq@amazon.com>
2023-09-27 18:20:46 -0400

    Added new images into data.js and updated descriptions

--- src/data.js
+++ src/data.js
@@ -1,11 +1,88 @@
-export const sliderItems = {
+export const sliderItems = [
+    {
+        id:1,
+        img: "https://drive.google.com/file/d/1RiBSYF7YYNSGkwRCeXEu1vSBdCXpiRAS/view",
+        title: "NEW ARRIVALS",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 25% OFF FOR NEW ARRIVALS.",
+        bg: "",
+    },
+    {
+        id:2,
+        img: "https://images.pexels.com/photos/9123011/pexels-photo-9123011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title: "AUTUMN SALE",
+        desc: "TIME TO SHOP 'TIL THE LEAVES DROP! DISCOUNTS UP TO 40% OFF.",
+        bg: "",
+    },
+    {
+        id:3,
+        img: "https://images.pexels.com/photos/5816424/pexels-photo-5816424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        title: "BLACK AND WHITE COLLECTION",
+        desc: "CHECK OUT OUR NEWEST AND EXCLUSIVE B&W COLLECTION.",
+        bg: "",
+    },
+    {
+        id:4,
+        img: "https://drive.google.com/file/d/1BzX7g7Nbu5GOHwHIMWa8vfG19LteaWXp/view",
+        title: "GRAPHIC TEES",
+        desc: "IT DOESN'T GET MORE O.G. THAN A SIMPLE GRAPHIC TEE",
+        bg: "",
+    },
+]
 
-}
+export const categories = [
+    {
+        id:1,
+        img: "https://images.pexels.com/photos/3048527/pexels-photo-3048527.png",
+        title: "B & W",
+    },
+    {
+        id:2,
+        img: "https://drive.google.com/file/d/1WdI3O8GbaiCmhMREaPcpBEtD3ftzoCMU/view",
+        title: "COZY COMFORT",
+    },
+    {
+        id:3,
+        img: "https://drive.google.com/file/d/16XpZCrFfp4Wno14Y6ImTOexGtunJV01X/view",
+        title: "GLOBAL TEES",
+    },
+    {
+        id:4,
+        img: "https://drive.google.com/file/d/1WV8VBgKz9kWb1mvdVWCCT35O77ggGKRT/view",
+        title: "WARM AND SELECT",
+    },
+]
 
-export const categories = {
-
-}
-
-export const popularProducts = {
-
-}
+export const popularProducts = [
+    {
+        id:1,
+        img: "",
+    },
+    {
+        id:2,
+        img: "",
+    },
+    {
+        id:3,
+        img: "",
+    },
+    {
+        id:4,
+        img: "",
+    },
+    {
+        id:5,
+        img: "",
+    },
+    {
+        id:6,
+        img: "",
+    },
+    {
+        id:7,
+        img: "",
+    },
+    {
+        id:8,
+        img: "",
+    },
+]
