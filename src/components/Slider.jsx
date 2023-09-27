e14b633316b6dbc3a59339ac9e744313ddf31289
Gilliard <sgityriq@amazon.com>
2023-09-27 17:17:16 -0400

    Fixedsome display issues and updated CSS properties for each component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,16 +4,79 @@ import styled from "styled-components";
 import { sliderItems } from "../data";
 import { mobile } from "../responsive";
 
-const Container = styled.div``;
-const Arrow = styled.div``;
-const Wrapper = styled.div``;
-const Slide = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.img``;
-const InfoContainer = styled.div``;
-const Title = styled.h1``;
-const Desc = styled.p``;
-const Button = styled.button``;
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+position: relative;
+overflow: hidden;
+`;
+
+const Arrow = styled.div`
+width: 50px;
+height: 50px;
+background-color: #fff7f7;
+border-radius: 50%;
+display: flex;
+align-items: center;
+justify-content: center;
+position: absolute;
+top: 0;
+bottom: 0;
+left: ${(props) => props.direction === "left" && "10px"};
+right: ${(props) => props.direction === "right" && "10px"};
+margin: auto;
+cursor: pointer;
+opacity: 0.5;
+z-index: 2;
+`;
+
+const Wrapper = styled.div`
+height: 100%;
+display: flex;
+transition: all 1.5s ease;
+transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+align-items: center;
+background-color: #${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+height: 100%;
+flex: 1;
+`;
+
+const Image = styled.img`
+height: 80%;
+`;
+
+const InfoContainer = styled.div`
+flex: 1;
+padding: 50px;
+`;
+
+const Title = styled.h1`
+font-size: 70px;
+`;
+
+const Desc = styled.p`
+margin: 50px 0px;
+font-size: 20px;
+font-weight: 500;
+letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+padding: 10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+`;
 
 
 
@@ -35,9 +98,9 @@ const Slider = () => {
             </Arrow>
             <Wrapper slideIndex={slideIndex}>
                 {sliderItems.map((item) => (
-                <Slide>
+                <Slide bg={item.bg} key={item.id}>
                     <ImgContainer>
-                    <Image />
+                    <Image src={item.img} />
                     </ImgContainer>
                     <InfoContainer>
                         <Title></Title>
@@ -51,5 +114,7 @@ const Slider = () => {
                 <ArrowRightOutlined />
             </Arrow>
         </Container>
-    )
-}
+    );
+};
+
+export default Slider;
