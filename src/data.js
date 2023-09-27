088b69118813d0d168d1c8b920c638795169124b
Gilliard <sgityriq@amazon.com>
2023-09-27 18:38:59 -0400

    Added more images into Assets folder and updated links in data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const sliderItems = [
     {
         id:1,
-        img: "https://drive.google.com/file/d/1RiBSYF7YYNSGkwRCeXEu1vSBdCXpiRAS/view",
+        img: "https://images.pexels.com/photos/3696755/pexels-photo-3696755.jpeg",
         title: "NEW ARRIVALS",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 25% OFF FOR NEW ARRIVALS.",
         bg: "",
     },
     {
         id:2,
-        img: "https://images.pexels.com/photos/9123011/pexels-photo-9123011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        img: "https://images.pexels.com/photos/9123011/pexels-photo-9123011.jpeg",
         title: "AUTUMN SALE",
         desc: "TIME TO SHOP 'TIL THE LEAVES DROP! DISCOUNTS UP TO 40% OFF.",
         bg: "",
     },
     {
         id:3,
-        img: "https://images.pexels.com/photos/5816424/pexels-photo-5816424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        img: "https://images.pexels.com/photos/5816424/pexels-photo-5816424.jpeg",
         title: "BLACK AND WHITE COLLECTION",
         desc: "CHECK OUT OUR NEWEST AND EXCLUSIVE B&W COLLECTION.",
         bg: "",
