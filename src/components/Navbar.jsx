bf278ff0a79fdffaaab4b055d799fd2007155350
Gilliard <sgityriq@amazon.com>
2023-09-27 16:00:14 -0400

    Imported needs icons and components into Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
 
 const Navbar = () => {
   return (
