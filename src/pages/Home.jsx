43c60b2cd825258d9b515c50aec54a0041c91d2e
Gilliard <sgityriq@amazon.com>
2023-09-27 15:57:16 -0400

    New Navbar component and imported into Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Slider />
+    </div>
+  )
+}
+
+export default Home
