84f718fbf0277466a305403cf7dd07513052227f
Gilliard <sgityriq@amazon.com>
2023-09-27 16:55:48 -0400

    Internal components created for Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,6 +4,21 @@ import styled from "styled-components";
 import { sliderItems } from "../data";
 import { mobile } from "../responsive";
 
+const Container = styled.div``;
+const Arrow = styled.div``;
+const Wrapper = styled.div``;
+const Slide = styled.div``;
+const ImgContainer = styled.div``;
+const Image = styled.img``;
+const InfoContainer = styled.div``;
+const Title = styled.h1``;
+const Desc = styled.p``;
+const Button = styled.button``;
+
+
+
+
+
 const Slider = () => {
     return (
         <Container>
