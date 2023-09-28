b92fe26744f2faa1904ac84258935f6677e5e50f
Gilliard <sgityriq@amazon.com>
2023-09-27 20:33:07 -0400

    Created structure for components in Footer.jsx and working on styling properties

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,4 +10,64 @@ import {
   import styled from "styled-components";
   import { mobile } from "../responsive";
 
+
+
+  const Footer = () => {
+    return (
+      <Container>
+        <Left>
+          <Logo>GLOBALLY DISCLOSED.</Logo>
+          <Desc>
+            Being globally disclosed is not just a mindset but a way of life to empower people to
+            embrace their unique identities and make a statement that resonates with the world. Celebrate
+            the beauty of diversity and let the confidence shine through.
+          </Desc>
+          <SocialContainer>
+            <SocialIcon color="3B5999">
+              <Facebook />
+            </SocialIcon>
+            <SocialIcon color="E4405F">
+              <Instagram />
+            </SocialIcon>
+            <SocialIcon color="55ACEE">
+              <Twitter />
+            </SocialIcon>
+            <SocialIcon color="E60023">
+              <Pinterest />
+            </SocialIcon>
+          </SocialContainer>
+        </Left>
+        <Center>
+          <Title>Useful Links</Title>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Men's Fashion</ListItem>
+            <ListItem>Women's Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
+        </Center>
+        <Right>
+          <Title>Contact</Title>
+          <ContactItem>
+            <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
+          </ContactItem>
+          <ContactItem>
+            <Phone style={{marginRight:"10px"}}/> +1 234 56 78
+          </ContactItem>
+          <ContactItem>
+            <MailOutline style={{marginRight:"10px"}} /> tyriqg_mgmt@gmail.com
+          </ContactItem>
+          <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+        </Right>
+      </Container>
+    );
+  };
+  
+  export default Footer;
   
