2c14b5e1310a9f03d91eca2494cd92ccc11dd88f
Gilliard <sgityriq@amazon.com>
2023-09-27 20:23:41 -0400

    Imported social icons from material-ui

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import {
+    Facebook,
+    Instagram,
+    MailOutline,
+    Phone,
+    Pinterest,
+    Room,
+    Twitter,
+  } from "@material-ui/icons";
+  import styled from "styled-components";
+  import { mobile } from "../responsive";
+
+  
