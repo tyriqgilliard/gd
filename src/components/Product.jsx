5c7f815acd6dfed83b01ef6a047ae5a54d97cdc2
Gilliard <sgityriq@amazon.com>
2023-09-27 20:39:24 -0400

    Updated possible internal components and structure and potential CSS styling structure

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,3 +4,25 @@ import {
     ShoppingCartOutlined,
   } from "@material-ui/icons";
   import styled from "styled-components";
+
+  const Product = ({ item }) => {
+    return (
+      <Container>
+        <Circle />
+        <Image src={item.img} />
+        <Info>
+          <Icon>
+            <ShoppingCartOutlined />
+          </Icon>
+          <Icon>
+            <SearchOutlined />
+          </Icon>
+          <Icon>
+            <FavoriteBorderOutlined />
+          </Icon>
+        </Info>
+      </Container>
+    );
+  };
+  
+  export default Product;
